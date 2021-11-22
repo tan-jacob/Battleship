9f746cd167da818441b386578cbb596874f26e7c
inyoungk10 <inyoungk10@gmail.com>
2021-11-21 23:45:54 -0800

    cors

--- Backend/app.js
+++ Backend/app.js
@@ -4,8 +4,9 @@ const PORT = process.env.PORT || 9000;
 const resource = '/API/v1';
 const adminURL = '/API/v1/admin';
 const app = express();
-const fs = require('fs');
-const http = require('http');
+const cors = require('cors');
+
+app.use(cors());
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -21,7 +22,7 @@ db.connect((err) => {
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
