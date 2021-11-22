e5f98ef7b96061866bd94e08943e934f9bd28dc4
inyoungk10 <inyoungk10@gmail.com>
2021-11-21 23:51:04 -0800

    cors header

--- Backend/app.js
+++ Backend/app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(function (req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
 });
 
