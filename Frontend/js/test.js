19fa24094f4cad67ba7d39f854e874d663a84829
inyoungk10 <inyoungk10@gmail.com>
2021-11-27 22:13:26 -0800

    comments endpoints

---
+++ Frontend/js/test.js
@@ -0,0 +1,36 @@
+function post() {
+    let testdiv = document.getElementById('res');
+
+    let pID = document.getElementById('pictureid').value;
+    let uID = document.getElementById('userid').value;
+    let com = document.getElementById('comment').value;
+
+    console.log("pid: " + pID + " uid: " + uID + " com: " + com); 
+
+    let url = `http://localhost:9000/comments/${pID}`;
+
+    axios.post(url, {
+        userID: uID,
+        pictureID: pID,
+        comment: com
+    }).then(res => {
+        let response = JSON.parse(res);
+        testdiv.innerHTML = response;
+    }).catch(function(error) {
+        console.log(error);
+      });
+}
+
+function get() {
+    let testdiv = document.getElementById('res');
+
+    let pID = document.getElementById('pictureid').value;
+    console.log(pID);
+    let url = `http://localhost:9000/comments/${pID}`;
+
+    axios.get(url).then(res => {
+        console.log(res.data);
+    }).catch(function(error) {
+        console.log(error);
+      });
+}
