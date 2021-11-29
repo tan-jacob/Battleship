0bc3407a63078659962fc7bdf74517db4757f5cb
inyoungk10 <inyoungk10@gmail.com>
2021-11-28 23:17:40 -0800

    remove window location

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,5 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-<<<<<<< HEAD
-=======
-
->>>>>>> fbe1567413c0102bacf1fcdf4a38a9dffb0369b2
 const express = require('express');
 const axios = require('axios');
 const mysql = require('mysql');
