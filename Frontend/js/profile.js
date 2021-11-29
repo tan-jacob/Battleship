32cf713d0714a6212b5c14224eada8a27e9c807e
inyoungk10 <inyoungk10@gmail.com>
2021-11-28 23:55:05 -0800

    public

--- Frontend/js/profile.js
+++ Frontend/js/profile.js
@@ -11,10 +11,42 @@ axios.get(url).then(res => {
     console.log(data);
     for (i = 0; i < data.length; i++) {
         let p = document.createElement('p');
+        let div =document.createElement('div');
+
         p.innerHTML = `Comment made on picture ${res.data[i].pictureID}: ${res.data[i].comment}`;
-        commentsDiv.appendChild(p);
+
+        let editButton = document.createElement('input');
+        editButton.type = 'button';
+        editButton.onclick = editComment(res.data[i].commentID);
+        editButton.value = "Edit Comment";
+
+        let deleteButton = document.createElement('input');
+        deleteButton.type = 'button';
+        deleteButton.onclick = deleteComment(res.data[i].commentID);
+        deleteButton.value = "Delete Comment";
+
+        div.appendChild(p);
+        div.appendChild(editButton);
+        div.appendChild(deleteButton);
+
+        commentsDiv.appendChild(div);
     }
     
 }).catch(function(error) {
     console.log(error);
 });
+
+function editComment(comID) {
+
+}
+
+function deleteComment(comID) {
+    let url = `http://localhost:8888/api/v1/cat/comments/delete/${comID}`;
+
+    axios.delete(url).then(res => {
+        let response = JSON.parse(res);
+        console.log(response);
+    }).catch(function(error) {
+        console.log(error);
+    });
+}
